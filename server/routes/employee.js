d9db5e617498556ba1ea299eb673fd613ceadc71
Anshul <ananyaj138@gmail.com>
2024-09-12 18:03:38 +0530

    'Server commit'

--- server/routes/employee.js
+++
@@ -1,7 +0,0 @@
-import express from 'express'
-const EmployeeRouter = express.Router();
-import {addEmployee,signInEmployee} from '../controller/employeeController.js'
-import validateAdminToken from '../middelware/adminAuthentication.js';
-EmployeeRouter.post('/add-employee',validateAdminToken,addEmployee);
-EmployeeRouter.post('/signin',signInEmployee)
-export default EmployeeRouter;
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,4 @@
 import express from 'express';
 const router=express.Router();
-import departmentRouter from '../routes/department.js';
-import EmployeeRouter from '../routes/employee.js'
-
-router.use('/departments',departmentRouter);
-router.use('/',EmployeeRouter)
 
 export default router;
